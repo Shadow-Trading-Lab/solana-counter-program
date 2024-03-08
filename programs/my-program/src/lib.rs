b4263a05f955c0e0c2044bb313067c8bd5926ada
ak8893893 <ak8893893@yahoo.com.tw>
2024-03-08 18:46:16 +0800

    v3-comment

--- programs/my-program/src/lib.rs
+++ programs/my-program/src/lib.rs
@@ -1,11 +1,13 @@
 use anchor_lang::prelude::*;
 
+// 定義智能合約的ID
 declare_id!("9KHoG32S9i4RWc5Qp4z3EDLDfwiQ3uZHQSdubyLsAJF");
 
 #[program]
 pub mod my_program {
     use super::*;
 
+    // 初始化函數，設定計數器的初始值
     pub fn initialize(ctx: Context<Initialize>, value: u64) -> Result<()> {
         let my_counter = &mut ctx.accounts.my_counter;
         my_counter.value = value;
@@ -15,7 +17,9 @@ pub mod my_program {
         Ok(())
     }
 
+    // 加上一個偶數的函數
     pub fn add_even(ctx: Context<AddEven>, value: u64) -> Result<()> {
+        // 需求檢查：確保輸入的值是偶數
         require!(value % 2 == 0, ErrorCode::ValueIsNotEven);
         let my_counter = &mut ctx.accounts.my_counter;
         my_counter.value = my_counter.value.checked_add(value).unwrap();
@@ -24,7 +28,9 @@ pub mod my_program {
         Ok(())
     }
 
+    // 減去一個奇數的函數
     pub fn minus_odd(ctx: Context<MinusOdd>, value: u64) -> Result<()> {
+        // 需求檢查：確保輸入的值是奇數
         require!(value % 2 == 1, ErrorCode::ValueIsNotOdd);
         let my_counter = &mut ctx.accounts.my_counter;
         my_counter.value = my_counter.value.checked_sub(value).unwrap();
@@ -33,7 +39,9 @@ pub mod my_program {
         Ok(())
     }
 
+    // 乘以一個偶數的函數
     pub fn times_even(ctx: Context<TimesEven>, value: u64) -> Result<()> {
+        // 需求檢查：確保輸入的值是偶數
         require!(value % 2 == 0, ErrorCode::ValueIsNotEven);
         let my_counter = &mut ctx.accounts.my_counter;
         my_counter.value = my_counter.value.checked_mul(value).unwrap();
@@ -42,6 +50,7 @@ pub mod my_program {
         Ok(())
     }
 
+    // 關閉計數器的函數
     pub fn close(_ctx: Context<Close>) -> Result<()> {
         Ok(())
     }
@@ -49,7 +58,7 @@ pub mod my_program {
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    // space: 8 (discriminator) + MyCounter::MAX_SIZE
+    // 初始化計數器所需的空間大小定義
     #[account(init_if_needed, payer = user, seeds = [b"my-counter", user.key().as_ref()], bump, space = 8 + MyCounter::MAX_SIZE)]
     pub my_counter: Account<'info, MyCounter>,
     #[account(mut)]
@@ -86,12 +95,14 @@ pub struct Close<'info> {
     pub user: Signer<'info>,
 }
 
+// 定義計數器結構體
 #[account]
 pub struct MyCounter {
     pub value: u64,
     pub bump: u8,
 }
 
+// 定義錯誤代碼
 #[error_code]
 pub enum ErrorCode {
     #[msg("value is not even")]
@@ -101,6 +112,6 @@ pub enum ErrorCode {
 }
 
 impl MyCounter {
-    // space: 8 (u64) + 1 (bump)
-    const MAX_SIZE: usize = 8 + 1;
+    // 定義計數器最大空間大小
+    const MAX_SIZE: usize = 8 + 1; // u64型態值的空間加上一個u8型態的bump
 }
--- tests/my-program.ts
+++ tests/my-program.ts
@@ -4,100 +4,104 @@ import { MyProgram } from "../target/types/my_program";
 import { assert } from "chai";
 import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
 
+// 描述測試套件的名稱
 describe("my-program", () => {
-  // Configure the client to use the local cluster.
+  // 配置客戶端以使用本地集群
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
   const program = anchor.workspace.MyProgram as Program<MyProgram>;
+  // 獲取計數器公鑰
   const myCounter = anchor.web3.PublicKey.findProgramAddressSync(
     [
-      anchor.utils.bytes.utf8.encode("my-counter"),
-      provider.wallet.publicKey.toBuffer(),
+      anchor.utils.bytes.utf8.encode("my-counter"), // 將字符串“my-counter”編碼為字節
+      provider.wallet.publicKey.toBuffer(), // 獲取錢包公鑰的緩存
     ],
-    program.programId
+    program.programId // 指定程序ID
   )[0];
 
+  // 測試用例：創建並初始化我的計數器
   it("Creates and initializes my counter", async () => {
+    // 調用智能合約的初始化方法
     const txid = await program.methods
-      .initialize(new anchor.BN(1234))
+      .initialize(new anchor.BN(1234)) // 設定初始值為1234
       .accounts({
-        myCounter: myCounter,
-        user: provider.wallet.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
+        myCounter: myCounter, // 指定計數器賬戶
+        user: provider.wallet.publicKey, // 指定用戶錢包公鑰
+        systemProgram: anchor.web3.SystemProgram.programId, // 系統程序ID
       })
-      .rpc();
-      console.log("Initialize transaction signature:", txid);
-      console.log(`SolScan transaction link: https://solscan.io/tx/${txid}?cluster=devnet`);
+      .rpc(); // 發送交易
+    console.log("Initialize transaction signature:", txid);
+    console.log(`SolScan transaction link: https://solscan.io/tx/${txid}?cluster=devnet`);
 
-      // 驗證計數器賬戶的值是否正確設置為1234
-      const account = await program.account.myCounter.fetch(myCounter);
-      console.log("The account counter is",account.value.toString(10), "now ");
-      assert.ok(account.value.eq(new anchor.BN(1234)));
+    // 驗證計數器賬戶的值是否正確設置為1234
+    const account = await program.account.myCounter.fetch(myCounter);
+    console.log("The account counter is",account.value.toString(10), "now ");
+    assert.ok(account.value.eq(new anchor.BN(1234)));
   });
 
+  // 測試用例：向我的計數器加上一個偶數
   it("Add an even number to my counter", async () => {
     const txid = await program.methods
-      .addEven(new anchor.BN(420))
+      .addEven(new anchor.BN(420)) // 加上值420
       .accounts({
         myCounter: myCounter,
         user: provider.wallet.publicKey,
       })
       .rpc();
 
-      console.log("AddEven transaction signature:", txid);
-      console.log(`SolScan transaction link: https://solscan.io/tx/${txid}?cluster=devnet`);
+    console.log("AddEven transaction signature:", txid);
+    console.log(`SolScan transaction link: https://solscan.io/tx/${txid}?cluster=devnet`);
 
-      // 
-      const account = await program.account.myCounter.fetch(myCounter);
-      console.log("The account counter is",account.value.toString(10), "now ");
-      assert.ok(account.value.eq(new anchor.BN(1654)));
+    const account = await program.account.myCounter.fetch(myCounter);
+    console.log("The account counter is",account.value.toString(10), "now ");
+    assert.ok(account.value.eq(new anchor.BN(1654))); // 驗證加後的值
   });
 
+  // 測試用例：從我的計數器減去一個奇數
   it("Minus an odd number to my counter", async () => {
     const txid = await program.methods
-      .minusOdd(new anchor.BN(69))
+      .minusOdd(new anchor.BN(69)) // 減去值69
       .accounts({
         myCounter: myCounter,
         user: provider.wallet.publicKey,
       })
       .rpc();
 
-      console.log("MinusOdd transaction signature:", txid);
-      console.log(`SolScan transaction link: https://solscan.io/tx/${txid}?cluster=devnet`);
+    console.log("MinusOdd transaction signature:", txid);
+    console.log(`SolScan transaction link: https://solscan.io/tx/${txid}?cluster=devnet`);
 
-      // 
-      const account = await program.account.myCounter.fetch(myCounter);
-      console.log("The account counter is",account.value.toString(10), "now ");
-      assert.ok(account.value.eq(new anchor.BN(1585)));
+    const account = await program.account.myCounter.fetch(myCounter);
+    console.log("The account counter is",account.value.toString(10), "now ");
+    assert.ok(account.value.eq(new anchor.BN(1585))); // 驗證減後的值
   });
 
+  // 測試用例：將我的計數器乘以一個偶數
   it("Times an even number to my counter", async () => {
     const txid = await program.methods
-      .timesEven(new anchor.BN(2))
+      .timesEven(new anchor.BN(2)) // 乘以2
       .accounts({
         myCounter: myCounter,
         user: provider.wallet.publicKey,
       })
       .rpc();
 
-      console.log("TimesEven transaction signature:", txid);
-      console.log(`SolScan transaction link: https://solscan.io/tx/${txid}?cluster=devnet`);
+    console.log("TimesEven transaction signature:", txid);
+    console.log(`SolScan transaction link: https://solscan.io/tx/${txid}?cluster=devnet`);
 
-      // 
-      const account = await program.account.myCounter.fetch(myCounter);
-      console.log("The account counter is",account.value.toString(10), "now ");
-      assert.ok(account.value.eq(new anchor.BN(3170)));
+    const account = await program.account.myCounter.fetch(myCounter);
+    console.log("The account counter is",account.value.toString(10), "now ");
+    assert.ok(account.value.eq(new anchor.BN(3170))); // 驗證乘後的值
   });
 
+  // 測試用例：關閉我的計數器賬戶
   it("Close my counter account", async () => {
     const txid = await program.methods
-      .close()
+      .close() // 調用關閉賬戶的方法
       .accounts({
         myCounter: myCounter,
         user: provider.wallet.publicKey,
       })
       .rpc();
-
 
     console.log("Close counter account transaction signature:", txid);
     console.log(`SolScan transaction link: https://solscan.io/tx/${txid}?cluster=devnet`);
